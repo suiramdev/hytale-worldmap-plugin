530cd129829ccb7da7bd5c410af8067b5666bbdf
suiramdev <me@suiram.dev>
2026-01-14 01:02:06 +0100

    Refactor project to create a Hytale worldmap plugin
    Updated project metadata to reflect the new plugin name and description. Introduced the main plugin class, Worldmap, and organized the project structure with placeholder directories for commands, config, listeners, services, storage, and utils. Adjusted the manifest file to align with the new plugin details.

--- src/main/java/com/worldmap/worldmap/Worldmap.java
+++ src/main/java/com/worldmap/worldmap/Worldmap.java
@@ -5,50 +5,50 @@ package com.example.templateplugin;
  * 
  * TODO: Implement your plugin logic here.
  * 
- * @author YourName
+ * @author suiramdev
  * @version 1.0.0
  */
-public class TemplatePlugin {
+public class Worldmap {
 
-    private static TemplatePlugin instance;
-    
+    private static Worldmap instance;
+
     /**
      * Constructor - Called when plugin is loaded.
      */
-    public TemplatePlugin() {
+    public Worldmap() {
         instance = this;
         System.out.println("[TemplatePlugin] Plugin loaded!");
     }
-    
+
     /**
      * Called when plugin is enabled.
      */
     public void onEnable() {
-        System.out.println("[TemplatePlugin] Plugin enabled!");
-        
+        System.out.println("[Worldmap] Plugin enabled!");
+
         // TODO: Initialize your plugin here
         // - Load configuration
         // - Register event listeners
         // - Register commands
         // - Start services
     }
-    
+
     /**
      * Called when plugin is disabled.
      */
     public void onDisable() {
-        System.out.println("[TemplatePlugin] Plugin disabled!");
-        
+        System.out.println("[Worldmap] Plugin disabled!");
+
         // TODO: Cleanup your plugin here
         // - Save data
         // - Stop services
         // - Close connections
     }
-    
+
     /**
      * Get plugin instance.
      */
-    public static TemplatePlugin getInstance() {
+    public static Worldmap getInstance() {
         return instance;
     }
 }
